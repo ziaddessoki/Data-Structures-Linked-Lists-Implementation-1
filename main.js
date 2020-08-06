5245faaaeeedadf4b49af6c13fb988aaca0f2e17
Zee(Ziad) Eldesouky <ziad.dessoki@yahoo.com>
2020-08-06 02:51:36 +0000

    append and prepend

--- main.js
+++ main.js
@@ -13,22 +13,42 @@
 // };
 
 class LinkedList {
-  constructor(value) {
-    this.head = {
-      value: value,
+  constructor(value){
+    this.head={
+      value:value,
       next: null
-    };
+    }
     this.tail = this.head;
-    this.length = 1;
+    this.length = 1
   }
-  append(value) {
-    //Code here
+  append(value){
+    const newNode = {
+      value:value,
+      next:null
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this
+  }
+
+  prepend(value){
+     const newNode = {
+      value:value,
+      next:null
+    }
+    newNode.next = this.head
+    this.head =newNode;
+    this.length++;
+    return this;
   }
 }
 
 let myLinkedList = new LinkedList(10);
 myLinkedList.append(5);
 myLinkedList.append(16);
+myLinkedList.append(7);
+myLinkedList.prepend(17);
 
 
 
