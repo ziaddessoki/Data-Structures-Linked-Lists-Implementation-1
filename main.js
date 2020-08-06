5bf831ba2257f1fe2b6cc61b76a09f27ea84d396
Zee(Ziad) Eldesouky <ziad.dessoki@yahoo.com>
2020-08-06 02:07:55 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+// Create the below linked list:
+// myLinkedList = {
+//   head: {
+//     value: 10
+//     next: {
+//       value: 5
+//       next: {
+//         value: 16
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    //Code here
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+
+
+
